e108e984b1669a660ee9f506e2d621103f1b44cc
Dmitriy1179 <chda7911@gmail.com>
2021-02-01 21:08:39 +0200

    services

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const SliderСomponent = (function() {
     "images/Rectangle4.png",
   ]
   const imgElement = document.createElement("img");
-  imgElement.setAttribute("style", "height: 100%; width: 100%; object-fit: cover");
+  imgElement.setAttribute("style", "height: 100%; width: 100%; object-fit: cover;");
   const domElement = document.querySelector("#slideEl");
   imgElement.src = arrImages[image]
   domElement.appendChild(imgElement)
@@ -41,38 +41,45 @@ const SliderСomponent = (function() {
   const nextButtonElement = document.querySelector("#nextButton")
   const prevButtonElement = document.querySelector("#prevButton")
   labelPrvBtnElement[0].style.opacity = 0.5
+  labelPrvBtnElement[0].style.cursor = "default"
   prevButtonElement.style.disabled = true 
 
   return {
     nextSlide: function() {
       if (image == arrImages.length - 1) {
         labelNxtBtnElement[0].style.opacity = 0.5
+        labelNxtBtnElement[0].style.cursor = "default"
         nextButtonElement.style.disabled = true
       }
       else {
         image++;
         if (image == arrImages.length - 1) {
           labelNxtBtnElement[0].style.opacity = 0.5
+          labelNxtBtnElement[0].style.cursor = "default"
           nextButtonElement.style.disabled = true
         }
       }
       labelPrvBtnElement[0].style.opacity = 1
+      labelPrvBtnElement[0].style.cursor = "pointer"
       prevButtonElement.style.disabled = false 
       imgElement.src = arrImages[image];
     },
     prevSlide: function() {
       if (image == 0) {
         labelPrvBtnElement[0].style.opacity = 0.5
+        labelPrvBtnElement[0].style.cursor = "default"
         prevButtonElement.style.disabled = true 
       }
       else {
         image--;
         if (image == 0) {
           labelPrvBtnElement[0].style.opacity = 0.5
+          labelPrvBtnElement[0].style.cursor = "default"
           prevButtonElement.style.disabled = true 
         }
       }
       labelNxtBtnElement[0].style.opacity = 1
+      labelNxtBtnElement[0].style.cursor = "pointer"
       nextButtonElement.style.disabled = false
       imgElement.src = arrImages[image];
     },

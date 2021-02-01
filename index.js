6670ff422da3cf9c10dc61a6db68a52887c487fc
Dmitriy1179 <chda7911@gmail.com>
2021-02-01 12:26:04 +0200

    slider js

--- index.js
+++ index.js
@@ -23,37 +23,57 @@ function attachEvent (event, handler, node) {
 
 attachEvent("click", jumpUp, document.querySelector("#upbutton"))
 
-const arrImages = [
-  "images/Rectangle3.png",
-  "images/Rectangle2.png",
-  "images/Rectangle4.png",
-]
 
 const SliderСomponent = (function() {
   var image = 0;
+  const arrImages = [
+    "images/Rectangle3.png",
+    "images/Rectangle2.png",
+    "images/Rectangle4.png",
+  ]
   const imgElement = document.createElement("img");
   imgElement.setAttribute("style", "height: 100%; width: 100%; object-fit: cover");
   const domElement = document.querySelector("#slideEl");
   imgElement.src = arrImages[image]
   domElement.appendChild(imgElement)
+  const labelPrvBtnElement = document.getElementsByName("labelPrvBtn")
+  const labelNxtBtnElement = document.getElementsByName("labelNxtBtn")
+  const nextButtonElement = document.querySelector("#nextButton")
+  const prevButtonElement = document.querySelector("#prevButton")
+  labelPrvBtnElement[0].style.opacity = 0.5
+  prevButtonElement.style.disabled = true 
 
   return {
     nextSlide: function() {
-      if (image < arrImages.length - 1) {
-        image++;
+      if (image == arrImages.length - 1) {
+        labelNxtBtnElement[0].style.opacity = 0.5
+        nextButtonElement.style.disabled = true
       }
       else {
-        image = 0
+        image++;
+        if (image == arrImages.length - 1) {
+          labelNxtBtnElement[0].style.opacity = 0.5
+          nextButtonElement.style.disabled = true
+        }
       }
+      labelPrvBtnElement[0].style.opacity = 1
+      prevButtonElement.style.disabled = false 
       imgElement.src = arrImages[image];
     },
     prevSlide: function() {
-      if (image > 0) {
-        image--;
+      if (image == 0) {
+        labelPrvBtnElement[0].style.opacity = 0.5
+        prevButtonElement.style.disabled = true 
       }
       else {
-        image = arrImages.length - 1
+        image--;
+        if (image == 0) {
+          labelPrvBtnElement[0].style.opacity = 0.5
+          prevButtonElement.style.disabled = true 
+        }
       }
+      labelNxtBtnElement[0].style.opacity = 1
+      nextButtonElement.style.disabled = false
       imgElement.src = arrImages[image];
     },
   }
